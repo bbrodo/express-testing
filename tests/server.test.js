a265912d71d611d2260b78b67ddefdc25164c456
bbrodo <15292@coderacademy.edu.au>
2024-10-31 14:14:32 +1100

    feat: additional test example

--- tests/server.test.js
+++ tests/server.test.js
@@ -6,5 +6,12 @@ describe("root route works", () => {
     const response = await request(app).get("/");
 
     expect(response.body.message).toBe("hello world");
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("server returns a response with no auth headers because we are not sending auth data", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.headers["Authorization"]).toBeFalsy();
   });
 });
